fbe9b8f7685bf0b0130625ff5929e1fa712f0c4f
nirnanaaa <mosny@zyg.li>
2013-05-28 21:45:26 +0200

    added lowercased username to users

--- db/migrate/20130528194413_add_username_lower_to_users.rb
+++ db/migrate/20130528194413_add_username_lower_to_users.rb
@@ -1,5 +1,11 @@
 class AddUsernameLowerToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :username_lower, :string
-  end
+    def up
+      add_column :users, :username_lower, :string, limit: 20
+      execute "update users set username_lower = lower(username)"
+      add_index :users, [:username_lower], unique: true
+      change_column :users, :username_lower, :string, limit: 20, null:false
+    end
+    def down
+      remove_column :users, :username_lower
+    end
 end
