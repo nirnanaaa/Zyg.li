6bd348a796a30d46b822c5239f87083619b81745
nirnanaaa <mosny@zyg.li>
2013-05-28 19:13:31 +0200

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BSH::Application.config.secret_key_base = 'adcbc0743e3bc07da3be076721f6c30dbe083c3c22b338056b6c776f129946722e090c90f26af70162f27058b5208cf76ad877d432ae321c5215c6b0c9dc060d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BSH::Application.config.session_store :cookie_store, key: '_BSH_session'
