e8f79faba2213142edb09dd791389860e1a19673
nirnanaaa <mosny@zyg.li>
2013-05-28 21:58:36 +0200

    user <=> group relation

--- db/migrate/20130528195212_create_group_members.rb
+++ db/migrate/20130528195212_create_group_members.rb
@@ -3,8 +3,7 @@ class CreateGroupMembers < ActiveRecord::Migration
     create_table :group_members do |t|
       t.integer :group_id
       t.integer :user_id
-
-      t.timestamps
     end
+    add_index :group_members, [:group_id, :user_id], :unique => true, :name => 'index_group_members_on_group_id_and_user_id'
   end
 end
