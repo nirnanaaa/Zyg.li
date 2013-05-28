8ea0e0853875391a5b0593c3c68cc0e7149b597b
nirnanaaa <mosny@zyg.li>
2013-05-28 21:43:19 +0200

    added indexes and uniqueness

--- db/migrate/20130528194159_add_display_username_to_users.rb
+++ db/migrate/20130528194159_add_display_username_to_users.rb
@@ -1,4 +1,14 @@
 class AddDisplayUsernameToUsers < ActiveRecord::Migration
-  def change
+  def up
+    add_column :users, :display_username, :string
+    execute "UPDATE users SET display_username = username"
+    execute "UPDATE users SET username = REPLACE(username, ' ', '')"
+    add_index :users, :username, :unique
+  end
+
+  def down
+    remove_index :users, :username
+    execute "UPDATE users SET username = display_username"
+    remove_column :users, :display_username
   end
 end
