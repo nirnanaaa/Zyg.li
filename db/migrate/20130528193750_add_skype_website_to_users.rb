7bbd6fbc8f2ff76584cc629bcd95eed046c3856a
nirnanaaa <mosny@zyg.li>
2013-05-28 21:40:51 +0200

    added down action for users model

--- db/migrate/20130528193750_add_skype_website_to_users.rb
+++ db/migrate/20130528193750_add_skype_website_to_users.rb
@@ -1,7 +1,12 @@
 class AddSkypeWebsiteToUsers < ActiveRecord::Migration
-  def change
+  def up
     add_column :users, :skype, :string
     add_column :users, :website, :string
     execute("UPDATE users SET website = '#{Rails.configuration.default_website}'")
+  end
+
+  def down
+    remove_column :users, :skype
+    remove_column :users, :website
   end
 end
