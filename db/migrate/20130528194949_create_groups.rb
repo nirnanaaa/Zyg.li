f7e6c0158be31ea14f79229a002a8fbcda8a619c
nirnanaaa <mosny@zyg.li>
2013-05-28 21:56:52 +0200

    added groups schema

--- db/migrate/20130528194949_create_groups.rb
+++ db/migrate/20130528194949_create_groups.rb
@@ -1,8 +1,18 @@
 class CreateGroups < ActiveRecord::Migration
-  def change
+  def up
     create_table :groups do |t|
-
+      t.boolean :automatic,   default: false, null: false
+      t.string  :name,        null: false
+      t.integer :user_count,  default: 0, null: false
+      
       t.timestamps
     end
+
+      execute <<SQL
+      ALTER SEQUENCE groups_id_seq START WITH 100
+SQL
+  end
+  def down
+    remove_column :groups, :automatic
   end
 end
