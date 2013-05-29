bc56758d25a450c3b7d5d11b4a572a5cfed91a24
nirnanaaa <mosny@zyg.li>
2013-05-29 23:09:59 +0200

    routes annotated

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,43 @@
+# == Route Map (Updated 2013-05-29 23:08)
+#
+#      Prefix Verb   URI Pattern                 Controller#Action
+# jasminerice        /jasmine                    Jasminerice::Engine
+#             GET    /404(.:format)              exceptions#not_found_404
+# sidekiq_web        /sidekiq                    Sidekiq::Web
+#       users GET    /users(.:format)            users#index
+#             POST   /users(.:format)            users#create
+#    new_user GET    /users/new(.:format)        users#new
+#   edit_user GET    /users/:id/edit(.:format)   users#edit
+#        user GET    /users/:id(.:format)        users#show
+#             PATCH  /users/:id(.:format)        users#update
+#             PUT    /users/:id(.:format)        users#update
+#             DELETE /users/:id(.:format)        users#destroy
+#      groups GET    /groups(.:format)           groups#index
+#             POST   /groups(.:format)           groups#create
+#   new_group GET    /groups/new(.:format)       groups#new
+#  edit_group GET    /groups/:id/edit(.:format)  groups#edit
+#       group GET    /groups/:id(.:format)       groups#show
+#             PATCH  /groups/:id(.:format)       groups#update
+#             PUT    /groups/:id(.:format)       groups#update
+#             DELETE /groups/:id(.:format)       groups#destroy
+#     members GET    /members(.:format)          members#index
+#             POST   /members(.:format)          members#create
+#  new_member GET    /members/new(.:format)      members#new
+# edit_member GET    /members/:id/edit(.:format) members#edit
+#      member GET    /members/:id(.:format)      members#show
+#             PATCH  /members/:id(.:format)      members#update
+#             PUT    /members/:id(.:format)      members#update
+#             DELETE /members/:id(.:format)      members#destroy
+#             GET    /robots.txt(.:format)       crawlers#robots
+#        root GET    /                           pages#index
+#
+# Routes for Jasminerice::Engine:
+#            GET /spec/:spec_id/fixtures/*filename(.:format) jasminerice/spec#fixtures
+# spec_index GET /spec(.:format)                             jasminerice/spec#index
+#            GET /fixtures/*filename(.:format)               jasminerice/spec#fixtures
+#            GET /(:suite)(.:format)                         jasminerice/spec#index
+#
+
 require 'sidekiq/web'
 
 require_dependency 'admin_constraint'
