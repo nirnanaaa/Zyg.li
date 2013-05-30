78dc5d954af56391b8c0c2ff3dabc61fa1a69c5b
nirnanaaa <mosny@zyg.li>
2013-05-30 10:43:56 +0200

    arch serializer extension

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
+require 'current_user'
+require 'archetype'
+require 'canonical_url'
+
 class ApplicationController < ActionController::Base
+  include CurrentUser
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
 #  protect_from_forgery with: :exception
@@ -6,5 +11,12 @@ class ApplicationController < ActionController::Base
 #    unless [ ActiveRecord::RecordNotFound, ActionController::RoutingError,
 #             ActionController::UnknownController, AbstractController::ActionNotFound].include? exception.class
 #    end
+
+  protect_from_forgery with: :null_session
+
+  before_filter :check_ajax
+  before_filter :set_locale
+#  before_filter
+
 
 end
---
+++ lib/archetype.rb
@@ -0,0 +1,40 @@
+class Archetype
+  include ActiveModel::Serialization
+
+  attr_accessor :id, :options
+
+  def initialize(id, options)
+    @id = id
+    @options = options
+  end
+
+  def attributes
+    {'id' => @id,
+     'options' => @options}
+  end
+
+  def self.default
+    'regular'
+  end
+
+  def self.poll
+    'poll'
+  end
+
+  def self.private_message
+    'private_message'
+  end
+
+  def self.list
+    return [] unless @archetypes.present?
+    @archetypes.values
+  end
+
+  def self.register(name, options={})
+    @archetypes ||= {}
+    @archetypes[name] = Archetype.new(name, options)
+  end
+  register 'regular'
+  register 'private_message'
+
+end
