08f659f704da4770f16b3b22bf5c4895500ccfcc
nirnanaaa <mosny@zyg.li>
2013-05-30 10:34:47 +0200

    page root controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,22 @@
+require_dependency 'page_serializer'
+
 class PagesController < ApplicationController
+  before_filter :ensure_current_user, except: [ :index, :show ]
+  skip_before_filter :check_ajax, only: [ :index ]
+
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,15 @@
 class UserSerializer < RootSerializer
+  attributes :id, 
+             :username,
+             :email,
+             :bio,
+             :created_at,
+             :updated_at,
+             :last_posted_at,
+             :website,
+             :skype
+
+#  has_one :approved_by, embed: :object, serializer: UserSerializer
+
   
 end
