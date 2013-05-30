6cd2fb9e191dc5e35c86aea8123fc8ff5e9ac098
nirnanaaa <mosny@zyg.li>
2013-05-30 18:50:02 +0200

    locale

--- app/helpers/js_locale_helper.rb
+++ app/helpers/js_locale_helper.rb
@@ -7,7 +7,7 @@ module JsLocaleHelper
     translations = YAML::load(File.open("#{Rails.root}/config/locales/client.#{locale_str}.yml"))
     #admin_contents = translations[locale_str].delete('admin_js')
 
-    translations[locale_str]['js'].merge!(admin_contents) if admin_contents.present?
+    #translations[locale_str]['js'].merge!(admin_contents) if admin_contents.present?
     result  = "I18n.translations = #{translations.to_json};\n"
     result << "I18n.locale = '#{locale_str}'\n"
     result
