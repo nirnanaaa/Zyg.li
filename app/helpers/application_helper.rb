ce371c6b3cb4fb443bc00800dac04f55b2723ecb
nirnanaaa <mosny@zyg.li>
2013-05-30 12:38:47 +0200

    modified html

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
+require 'current_user'
+require 'canonical_url'
+
 module ApplicationHelper
   def only_dev(text)
     text.html_safe if Rails.env.development?
@@ -6,5 +9,6 @@ module ApplicationHelper
   def only_prod(text)
     text.html_safe if Rails.env.production?
   end
+
 
 end
