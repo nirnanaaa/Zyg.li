2c119645d737e34bb1319c927279348b630c32b3
nirnanaaa <mosny@zyg.li>
2013-05-30 11:18:08 +0200

    moved not logged in exception

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe PagesController do
+  describe "access" do
+    it "disallows POST requests for not logged in users" do
+      expect do
+        post
+      end.to raise_error BSH::NotLoggedIn
+    end
+  end
 
 end
