39edd7c1250f7f444de73968b0bf965b2a658ad4
nirnanaaa <mosny@zyg.li>
2013-05-30 11:05:53 +0200

    added maintenance check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,16 +16,54 @@ class ApplicationController < ActionController::Base
 
   before_filter :check_ajax
   before_filter :set_locale
+  #before_filter :check_if_maintenance
 
   def set_locale
     I18n.locale = extract_locale_from_accept_language_header
   end
+  
+
+  def store_preloaded(key, json)
+    @preloaded ||= {}
+    @preloaded[key] = json.gsub("</", "<\\/")
+  end
+
+  def preload_json
+    if request.format and request.format.html?
+      store_preloaded('current_user', MultiJson.dump(CurrentUserSerializer.new(current_user, root:false)))
+    end
+  end
+
+  def serialize_data(obj, serializer, opts={})
+    serializer_opts = {scope: guardian}.merge!(opts)
+    if obj.is_a?(Array)
+      serializer_opts[:each_serializer] = serializer
+      ActiveModel::ArraySerializer.new(obj, serializer_opts).as_json
+    else
+      serializer.new(obj, serializer_opts).as_json
+    end
+  end
 
   protected
+
+
   def extract_locale_from_accept_language_header
     request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
   end
-#  before_filter
+
+  def check_if_maintenance
+  
+  end
+
+  def check_ajax
+    # not supporting anything else right now
+    return if !request.get?
+    raise RenderNil.new unless ((request.format && request.format.json?) || request.xhr?)
+  end
+
+  def ensure_current_user
+    raise BSH::NotLoggedIn.new unless current_user.present?
+  end
 
 
 end
