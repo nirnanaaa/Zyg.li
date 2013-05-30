289c2c472765e51b550874d83487a45b61480cec
nirnanaaa <mosny@zyg.li>
2013-05-30 11:12:25 +0200

    added application controller spec

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe ApplicationController do
+  let(:not_correct_api_key){ SecureRandom.hex(32) }
+
+  it "allows GET for all users" do
+  end
+
+  it "disallows PUT for non-api users" do
+    expect do
+    end.to raise_error BSH::NotLoggedIn
+  end
+
+
+end
